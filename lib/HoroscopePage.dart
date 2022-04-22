b8ee142183c96eec5c3ee6e0fbe54408c0b3b6b1
jhunanthonysacapano <jhunanthonysacapano@gmail.com>
2022-04-22 15:46:01 +0800

    ADD HOROSCOPE PAGE

---
+++ lib/HoroscopePage.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+
+
+
+
+// ignore: use_key_in_widget_constructors
+class HoroscopePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HoroscopePageState();
+  }
+}
+
+class HoroscopePageState extends State<HoroscopePage> {
+ 
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF100F40),
+      body: Container(
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
+import 'package:apotelesma/HoroscopePage.dart';
 import 'package:flutter/material.dart';
-
-
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -27,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: "Apotelesma"),
+      home: HoroscopePage(),
     );
   }
 }
